dff7145146e5de09cb03e50d894812d86eed4a5d
Andres Torres <andrestor2@gmail.com>
2021-06-17 19:15:57 -0500

    Add some details of menu

--- src/app/feature/menus/menus.component.ts
+++ src/app/feature/menus/menus.component.ts
@@ -16,6 +16,8 @@ export class MenusComponent implements OnInit {
   rawMenu?: any;
   username?: string;
   password?: string;
+  lastEditTime?: Date;
+  creationTime?: Date;
   menuLoaded = false;
 
   constructor(
@@ -33,29 +35,41 @@ export class MenusComponent implements OnInit {
   loadMenu(useExternal: boolean) {
     let localToken = localStorage.getItem(AppConstants.EXTERNAL_TOKEN);
     this.rawMenu = localStorage.getItem(AppConstants.EXTERNAL_MENU);
+
     if (!localToken || !this.rawMenu || useExternal) {
       if (this.username && this.password) {
         console.log('Load Data');
+        this.rawMenu = { menu: '', lastEditTime: '', creationTime: '' };
         this.menuLoaded = false;
         this.dataClientService.getTokenClient(this.username, this.password).subscribe((res: any) => {
           localStorage.setItem(AppConstants.EXTERNAL_TOKEN, res.waiterioToken);
           localToken = res.waiterioToken;
           if (localToken) {
             this.dataClientService.getMenuClient(localToken).subscribe((res: any) => {
-              this.rawMenu = res.menus[0].categories;
+              this.rawMenu.menu = res.menus[0].categories;
+              this.rawMenu.lastEditTime = new Date(res.menus[0].lastEditTime);
+              this.rawMenu.creationTime = new Date(res.menus[0].creationTime);
+              
+              this.lastEditTime = this.rawMenu.lastEditTime;
+              this.creationTime = this.rawMenu.creationTime;
               localStorage.setItem(AppConstants.EXTERNAL_MENU, JSON.stringify(this.rawMenu));
 
-              this.processedMenu = this.menuService.getGeneralData(this.rawMenu);
+              this.processedMenu = this.menuService.getGeneralData(this.rawMenu.menu);
               this.menuLoaded = true;
             });
           }
         });
       }
     } else {
-      
+
       this.menuLoaded = false;
-      this.processedMenu = this.menuService.getGeneralData(JSON.parse(this.rawMenu));
+      this.rawMenu = JSON.parse(this.rawMenu);
+      this.lastEditTime = this.rawMenu.lastEditTime;
+      this.creationTime = this.rawMenu.creationDate;
+      this.processedMenu = this.menuService.getGeneralData(this.rawMenu.menu);
       this.menuLoaded = true;
     }
   }
+
+
 }
